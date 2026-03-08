620c1f67d6232360faecc85c81003e494375d14f
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-08 22:32:07 +0900

    minimal server
    minimal server, that serves index.html
    middleware that accepts matching content-type of json. And, extracts
    path-to-file using builtin express modules, path's dirname and url's
    fileURLToPath.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // backup port is 5000
 
-console.log(`hello world`);
+// get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+// get the directory name from the file path
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Serving up the HTML file from the /public dir
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server has started on port: ${PORT}`);
