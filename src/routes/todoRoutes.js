9c2ddc3bb315be6fe9fe27ccd32e1b567a90c9a4
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-12 09:46:16 +0900

    get all todos when user login

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,16 +1,20 @@
-import express from "express";
-import db from "../db.js";
+import express from 'express'
+import db from '../db.js'
 
-const router = express.Router();
+const router = express.Router()
 
 // Get all todos for logged-in user
-router.get("/", (req, res) => {});
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
+})
 
 // Create new todo
-router.post("/", (req, res) => {});
+router.post('/', (req, res) => {})
 
 // Update a todo :id, is dynamic id, id of the user with matching todo
-router.put("/:id", (req, res) => {});
+router.put('/:id', (req, res) => {})
 
-router.delete("/:id", (req, res) => {});
-export default router;
+router.delete('/:id', (req, res) => {})
+export default router
