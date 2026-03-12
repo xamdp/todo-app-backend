f1a2bd85431da1fad87cdb99e637f17f61add9d2
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-12 09:40:41 +0900

    register user
    A user can register and credentials will be stored in database.
    The hashedPassword is saved instead of plain text password.
    When the user login, the password provided during login is rehashed and
    compared to the stored hashedPassword.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,25 +1,74 @@
-import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import db from "../db.js";
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
 
-const router = express.Router();
+const router = express.Router()
 
 // Register a new user endpoint /auth/register
-router.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  // do not save plain text passwords, but encrypt them using bcrypt package
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+    // do not save plain text passwords, but encrypt them using bcrypt package
 
-  const hashedPassword = bcrypt.hashSync(password, 8);
-  console.log(hashedPassword);
-  res.sendStatus(201);
-});
+    // encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    // save the new user and hashed password to the database
+    try {
+        const insertUser = db.prepare(
+            `INSERT INTO users (username, password) VALUES (?, ?)`
+        )
+        const result = insertUser.run(username, hashedPassword)
+
+        // now that we have a user, I want to add their  first todo for them
+        const defaultTodo = `Hello :D Add your first todo!`
+        const insertTodo = db.prepare(
+            `INSERT INTO todos (user_id, task) VALUES (?, ?)`
+        )
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign(
+            { id: result.lastInsertRowid },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        )
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+    console.log(hashedPassword)
+    res.sendStatus(201)
+})
 
 // Login a user /auth/login
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  // upon login, we encrypt the password in req.body, and compare it with encrypted registered password of the user
-  console.log(username, password);
-  sendStatus(200);
-});
-export default router;
+router.post('/login', (req, res) => {
+    const { username, password } = req.body
+    // upon login, we encrypt the password in req.body, and compare it with encrypted registered password of the user
+    try {
+        const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`)
+        const user = getUser.get(username)
+
+        // if we cannot find a user associated with that username, return out from the function
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' })
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        if (!passwordIsValid) {
+            return res.status(401).send({ message: 'Invalid password' })
+        }
+
+        // then we have a successful authentication
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: '24',
+        })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+    res.status(200).send({ message: `User is found` })
+})
+
+export default router
