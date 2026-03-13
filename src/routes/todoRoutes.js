5516b47acb0cdc37b8015903fc51b4b9ff3c82ca
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-13 11:09:25 +0900

    Add post, put, and delete todo routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -11,10 +11,39 @@ router.get('/', (req, res) => {
 })
 
 // Create new todo
-router.post('/', (req, res) => {})
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(
+        `INSERT INTO todos (user_id, task) VALUES (?, ?)`
+    )
+    const result = insertTodo.run(req.userId, task)
+
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
+})
 
 // Update a todo :id, is dynamic id, id of the user with matching todo
-router.put('/:id', (req, res) => {})
+router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
 
-router.delete('/:id', (req, res) => {})
+    const updatedTodo = db.prepare(
+        'UPDATE todos SET completed = ? WHERE id = ?'
+    )
+
+    updatedTodo.run(completed, id)
+
+    res.json({ message: 'Todo completed' })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(
+        `DELETE FROM todos WHERE id = ? AND user_id = ?`
+    )
+    deleteTodo.run(id, userId)
+    res.json({ message: `Todo with id of ${id} is deleted` })
+})
+
 export default router
